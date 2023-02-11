524967ba53c14704cb5140648d38e4d63e144386
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 <jade@jadecharles.com>
2023-02-11 12:09:33 -0800

    v0.1.13

---
+++ dist/index.js
@@ -0,0 +1,13 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+Object.defineProperty(exports, "default", {
+  enumerable: true,
+  get: function get() {
+    return _DateTime.default;
+  }
+});
+var _DateTime = _interopRequireDefault(require("./formatting/DateTime"));
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
