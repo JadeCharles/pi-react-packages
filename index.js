d614631a2c3f2a9d3682038d5189dd37f20f7432
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 <jade@jadecharles.com>
2023-02-11 10:50:00 -0800

    v0.1.7

---
+++ index.js
@@ -0,0 +1,9 @@
+import DateTime from './lib/formatting/DateTime';
+//import axios from './lib/axios.js';
+
+// This module is intended to unwrap Axios default export as named.
+// Keep top-level export same with static properties
+// so that it can keep same with es module or cjs
+
+export { DateTime as default };
+
