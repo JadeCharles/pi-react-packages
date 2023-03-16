2c70fd7784f1268cbf8dc8530124b0a20ed27f30
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 <jade@jadecharles.com>
2023-03-16 09:40:49 -0700

    v0.1.17

--- src/index.js
+++ src/index.js
@@ -1,5 +1,24 @@
-import DateTime from './formatting/DateTime';
+import DateTime from './common/ui/formatting/DateTime';
+import EmailDisplay from './common/ui/formatting/EmailDisplay';
+import NumberDisplay from './common/ui/formatting/NumberDisplay';
+import PhoneDisplay from './common/ui/formatting/PhoneDisplay';
+import StringDisplay from './common/ui/formatting/StringDisplay';
+import FormButton from './common/ui/buttons/FormButton';
+import ValidateModel from './common/ui/form-validator/ValidateModel';
+import ValidationResult from './common/ui/form-validator/ValidateResult';
+
+import FormValidator from './common/services/form-validator/FormValidator';
+import ContactForm from './messaging/ui/ContactForm';
 
 export {
-    DateTime as default
+    DateTime,
+    EmailDisplay,
+    NumberDisplay,
+    PhoneDisplay,
+    ContactForm,
+    FormButton,
+    FormValidator,
+    StringDisplay,
+    ValidateModel,
+    ValidationResult,
 };
