638ff55bd8e490b7f9d45654fc1e3bfc94f2684e
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 <jade@jadecharles.com>
2023-03-16 11:30:39 -0700

    v0.1.35

--- src/common/models/ConfigModel.js
+++ src/common/models/ConfigModel.js
@@ -1,11 +1,25 @@
 class ConfigModel { 
-    static companyId = "A32B21B2-1A90-4ECA-AFAA-6E8A08C60EDD";
-    static companyName = "Penumbra Labs";
-    static appName = "Pi Dark";
-    static companyNumber = 7;
+    static companyId = "";
+    static companyName = "";
+    static appName = "";
+    static companyNumber = 0;
     static defaultProfileImageUrl = "";
-    
+    static defaultPageTitle = "Home Page";
+
     static emptyId = "00000000-0000-0000-0000-000000000000";
+    static houseCompanyId = "A32B21B2-1A90-4ECA-AFAA-6E8A08C60EDD";
+
+    static init(companyName, companyId, companyNumber, appName = null, defaultProfileImageUrl = "") {
+        if (typeof companyName !== "string" || companyName.trim().length === 0) throw new Error("Invalid company name: " + companyName);
+        if (typeof companyId !== "string" || companyId.length !== 36 || companyId === ConfigModel.emptyId) throw new Error("Invalid company id. Must be a valid Guid: " + companyId);
+        if (typeof companyNumber !== "number" || companyNumber <= 0) throw new Error("Invalid company number. Must be a positive number: " + companyNumber);
+
+        ConfigModel.companyId = companyId;
+        ConfigModel.companyName = companyName;
+        ConfigModel.appName = appName || (companyName + " App");
+        ConfigModel.companyNumber = companyNumber;
+        ConfigModel.defaultProfileImageUrl = defaultProfileImageUrl;
+    }
 }
 
 export default ConfigModel;
