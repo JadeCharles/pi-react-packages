f512ea2cb139e02821d246d0e1348e66f76084f1
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 <jade@jadecharles.com>
2023-05-13 17:01:48 -0700

    Pre-public

---
+++ dist/geo/models/AddressModel.d.ts
@@ -0,0 +1,21 @@
+declare namespace Geo { 
+    type AddressModel = {
+        id: string | null;
+        street: string;
+        unit: string;
+        city: string;
+        state: string;
+        zip: string;
+        country: string;
+        lat: string;
+        lon: string;
+        status: number;
+        created: Date;
+        updated: Date;
+
+        toJson: () => { [key: string]: any };
+        fromJsonArray: (jsonArray: [any]) => AddressModel[];
+        countries: [{ [key: string]: string }];
+        states: [{ [key: string]: string }];
+    };
+}
