6cea96676324f88dc427df0962d0b3ee5610ffef
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 <jade@jadecharles.com>
2023-07-13 12:17:34 -0700

    HttpService error handling, v1.1.15 with axios

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,25 @@
 const path = require('path');
 
 module.exports = {
-  entry: './dist/index.js',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader'
-        }
-      }
-    ]
-  }
+    entry: './dist/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    // resolve: {
+    //     alias: {
+    //         axios: 'axios',
+    //     },
+    // },  
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            }
+        ]
+    }
 };
